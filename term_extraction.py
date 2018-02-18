4d1bcd12fd733abd36cfceba35fc5bdc02d0fe23
ssladam <35581233+ssladam@users.noreply.github.com>
2018-02-18 17:08:48 +1100

    Update term_extraction.py
    Properly filter intra-word punctuation

--- term_extraction.py
+++ term_extraction.py
@@ -1,70 +1,114 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-import itertools, nltk, string, re, os, docx
+import itertools, nltk, re, os, docx, unicodedata
 #If you've never setup nltk previously, execute the following line
 #nltk.download()
 
-corpus_path = "C:/temp/NU/453/Co11/" #location where all corpus .docx files are stored
-output_path =   "C:/temp/NU/453/" #location you will output all CSV files
+corpus_path = "C:/Users/adams/OneDrive/Documents/Northwestern/453 - Text/CS2/" #location where all corpus .docx files are stored
+output_path =   "C:/Users/adams/OneDrive/Documents/Northwestern/453 - Text/output/" #location you will output all CSV files
 
 #Preprocessing, opportunity to replace words or phrases with ECs
 #  DO NOT add "president" --> "presidentTrump" here, otherwise all instances
 #  of "president obama" would be converted to "presidentTrump obama"
 #  this should be used for fixed phrases ONLY
 phrase_dict = {"Trans-Pacific Partnership": "TPP",\
-          "North American Free Trade Agreement":"NAFTA",\
-          "World Trade Organization":"WTO",\
-          "fire and fury":"fireAndFury",\
-          #the "of" below only serves to force a split of "special counsel robert mueller"
-          "special counsel":"specialCounsel of",\
-          "lost jobs":"jobLosses",\
-          "job losses":"jobLosses",\
-          "chief of staff":"chiefOfStaff"
+            "North American Free Trade Agreement":"NAFTA",\
+            "World Trade Organization":"WTO",\
+            "fire and fury":"fireAndFury",\
+            "lost jobs":"jobLosses",\
+            "job losses":"jobLosses",\
+            "chief of staff":"chiefOfStaff",\
+            "E&E News":"EandENews",\
+            "S&P 500":"SandP500",\
+            "memorandum donald trump":"memorandum which donald trump",\
+            "Immigration and Nationality Act":"immigrationAndNationalityAct",\
+            "Club For Growth and Heritage":"clubForGrowthAndHeritage",\
+            "Freedom Caucus":"freedomCaucus",\
+            "Office of Personnel Management ":"officeOfPersonnel Management",\
+            "Office of Management and Budget":"officeOfManagementAndBudget",\
+            #Okay, I cheat just a bit by pre-filtering some terms that would be tricky to remove otherwise
+            #   these terms become "nested" with other terms, easier to just axe them here
+            "[sic]":"",\
+            "percent":"",\
+            "percentage":"",\
+            
           }
 
 #This will be swapped out after processing is complete, now you're safe to
 #  swap out individual words for their EC equivalent
 ec_dict = {'trump':'presidentDonaldTrump',\
                'president':'presidentDonaldTrump',\
-               "us":"UnitedStates",\
-               "u.s.":"UnitedStates",\
-               "mr. trump":"presidentDonaldTrump",\
+               "us":"unitedStates",\
+               "usa":"unitedStates",\
+               "united states":"unitedStates",\
                "mr trump":"presidentDonaldTrump",\
-               "mr. trumps":"presidentDonaldTrump",\
+               "mr trumps":"presidentDonaldTrump",\
                "president trump":"presidentDonaldTrump",\
+               "president trumps":"presidentDonaldTrump",\
                "President Donald Trump":"presidentDonaldTrump",\
                "Donald Trump":"presidentDonaldTrump",\
+               "president-elect donald trump":"presidentElectDonaldTrump",\
+               "presidentelect donald trumps":"presidentElectDonaldTrump",\
+               "presidentelect donald trump":"presidentElectDonaldTrump",\
+               "presidentelect donald j trump":"presidentElectDonaldTrump",\
+               "presidentelects":"presidentElectDonaldTrump",\
+               "presidentelect":"presidentElectDonaldTrump",\
                "Melania Trump":"melaniaTrump",\
                "trumps":"presidentDonaldTrump",\
                "us president donald trump":"presidentDonaldTrump",\
                "fbi director james comey":"jamesComey",\
                "comey":"jamesComey",\
-               "mr. rosss":"wilburRoss",\
-               "mr. ross":"wilburRoss",\
+               "mr rosss":"wilburRoss",\
+               "mr ross":"wilburRoss",\
                "commerce secretary wilbur ross":"wilburRoss",\
                "ross":"wilburRoss",\
-               "sen. dianne feinstein":"dianneFienstein",\
+               "sen dianne feinstein":"dianneFienstein",\
                "feinstein":"dianneFienstein",\
                "mueller":"robertMueller",\
                "robert mueller":"robertMueller",\
                "counsel robert mueller":"robertMueller",\
+               "special counsel":"robertMueller",\
                "eu":"europeanUnion",\
                "european union":"europeanUnion",\
-               "white house press secretary sarah huckabee sanders":"sarahHuckabeeSanders"
+               "white house press secretary sarah huckabee sanders":"sarahHuckabeeSanders",\
+               "trump administration":"trumpAdministration",\
+               "united nations":"unitedNations",\
+               "un":"unitedNations",\
+               "ina":"immigrationAndNationalityAct",\
+               "south korea":"southKorea",\
+               "south koreas":"southKorea",\
+               "ocare":"obamacare",\
+               "russias":"russia",\
+               "tariffs":"tariff",\
+               "us tariff":"tariff",\
+               "percent tariff":"tariff",\
+               "tariff hike":"tariff"
+
                
                }
 #the parser converts all entries to lower case, so we should do the same
 ec_dict = {key.lower(): value for key, value in ec_dict.items()}
 
 
-#add all words you want to filter into filter_words
+#add all words you want to filter into filter_words, this also occurs POST PROCESSING, so now if you filter
+#  the term "friday" it will drop that singular term, but WOULD NOT filter "friday night" since it doesn't match
 filter_words = set()
 filter_words.update(['year', 'week', 'month', 'day', 'hour', 'minute', 'time','past','morning','afternoon', 'evening', 'night'])
-filter_words.update(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
+filter_words.update(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday','today','yesterday','tomorrow'])
 filter_words.update(['january','february', 'march', 'april','may','june','july','august','september','october','november','december'])
+filter_words.update(['jan','feb', 'mar', 'apr','jun','jul','aug','sep','oct','nov','dec'])
 filter_words.update(['others', 'contrast','approach','change','response','people','possibility'])
-filter_words.update(['recommendation', 'side','control','fact','win','attempt','amid','something','e.g'])
+filter_words.update(['recommendation', 'side','control','fact','win','attempt','amid','something','e.g','isnt','ive','hes','im','ill'])
+filter_words.update(['rky','dc','dny','rfla','rid', 'try','weve','pas','co.','km','sens','rokla','am','cnn'])
+filter_words.update(['a','b','c','d','e','f','g','h'])
+filter_words.update(['']) #this was added, because the % symbol returns a valid single token, and is then stripped
+filter_words.update(['zero','one','two','three','four','five','six','seven','eight','nine'])
+#filter_words.update(set(nltk.corpus.stopwords.words('english'))
+#I intentionally didn't add STOP_WORDS to this list, otherwise it would filter mis-categorizations
+#   For example, "S&P 500" became "S", and "P", and 500 was lost. Only add this line when confident you've
+#   cleaned the articles
+
 
 
 #===========================================================
@@ -74,27 +118,29 @@ def lambda_unpack(f):
     return lambda args: f(*args)
 
 #============System to chunk individual terms into phrases ===================
-def extract_candidate_chunks(text, grammar=r'KT: {(<NN.*>+)? <NN.*>+}'):
-    # exclude candidates that are stop words or entirely punctuation
-    punct = set(string.punctuation)
+def extract_candidate_chunks(text, filter_words, grammar=r'KT: {(<NN.*>+)? <NN.*>+}'):
     #strip_text = re.sub(r"[^\-\,\.\?\!\'\(\)\w\d'\s]+",'',text) #remove unicode characters
-    strip_text = re.sub(r"[^\'\!\"\#\$\&\\\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\\\]\^\_\`\{\|\}\~\'\w\d'\s]+",'',text) #removes unicode punctuation 
-    #strip_text = text.encode('ascii', 'ignore')
-    #Note: we leave all "basic" punctuation to aid with sentence tokenization
+    #strip_text = re.sub(r"[^\'\!\"\#\$\&\\\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\\\]\^\_\`\{\|\}\~\'\w\d'\s]+",'',text) #removes unicode punctuation 
+    strip_text = unicodedata.normalize('NFKD', text).encode('ascii','ignore').decode('utf-8')
+    #Note: now I leave all punctuation to aid with sentence tokenization
     stop_words = set(nltk.corpus.stopwords.words('english'))
     stop_words |= filter_words
     
     # tokenize, POS-tag, and chunk using regular expressions
+    #todo: currently the grammar chunker incorrectly strings possessives, "trump's plan" --> "trumps plan"
+    #   ideally, that should be two tokens, "trump" and "plan"
     chunker = nltk.chunk.regexp.RegexpParser(grammar)
     tagged_sents = nltk.pos_tag_sents(nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(strip_text))
     all_chunks = list(itertools.chain.from_iterable(nltk.chunk.tree2conlltags(chunker.parse(tagged_sent))
                                                     for tagged_sent in tagged_sents))
     candidates = [' '.join(nltk.stem.WordNetLemmatizer().lemmatize(word) for word, pos, chunk in group).lower()
         for key, group in itertools.groupby(all_chunks, lambda_unpack(lambda word, pos, chunk: chunk != 'O')) if key]
-    return [cand for cand in candidates if cand not in stop_words]
-    #todo: why does the punct check filter EVERYTHING??
-    #return [cand for cand in candidates
-     #       if cand not in stop_words and not all(char in punct for char in cand)]
+    
+    #We had left standard punctuation to aid in sentence chunking. Now delete all intra-word punctuation. "f.b.i. --> "fbi"
+    for idx in range(len(candidates)):
+        candidates[idx] = re.sub(r"[!\"#$%&\'()*+,-./:;<=>?@\[\\\]^_`{|}~]", '', candidates[idx])
+
+    return [cand.lstrip() for cand in candidates if cand not in stop_words]
 
 #==========Take a single long string of text and convert it into terms / phrases========
 def parse_article(dsi_text, phrase_dict, ec_dict, grammar=r'KT: {(<NN.*>+)? <NN.*>+}'):
@@ -112,11 +158,12 @@ def parse_article(dsi_text, phrase_dict, ec_dict, grammar=r'KT: {(<NN.*>+)? <NN.
         ec_text += ' ' + sentence
     del sentence, phrase, ec, insensitive_phrase
     #Now that EC swapping is complete, let's chunk the terms
-    chunked_list = extract_candidate_chunks(ec_text, grammar)
+    chunked_list = extract_candidate_chunks(ec_text, filter_words, grammar)
     #We now have a series of all terms, in lower case. Now let's swap out EC terms
 
     chunked_series = pd.Series(chunked_list)
     chunked_series.replace(to_replace=ec_dict, inplace=True)
+    
     return pd.DataFrame(chunked_series, columns=['terms'])
 
 #===========================================================
